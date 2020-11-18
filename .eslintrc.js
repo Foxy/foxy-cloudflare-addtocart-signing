e7d42b4888ee58285a88b5fbf4b5edb5663b422b
ndvo <nelson@ocastudios.com>
2020-11-18 15:04:59 -0300

    chore: add foxy eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "@foxy.io",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
