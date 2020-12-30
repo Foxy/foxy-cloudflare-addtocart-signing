39389083240e4d5bae6aeac5a39bd54c962b6d93
ndvo <nelson@ocastudios.com>
2020-12-30 19:33:21 -0300

    test: import tests from SDK Signer

---
+++ test/mock/crypto.js
@@ -0,0 +1,16 @@
+/**
+ * Simple Mock Hmac class.
+ *
+ * Provides a sign method that
+ */
+export class MockHmac {
+
+  constructor() {
+    this.willFail = false
+  }
+
+  sign() {
+    return this.willFail ? Promise.reject() : Promise.resolve("signed");
+  }
+
+}
