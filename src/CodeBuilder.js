693c260a25e5ac118595ae1f751decaa3a6c350f
ndvo <nelson@ocastudios.com>
2021-01-05 16:12:57 -0300

    test(codebuilder): add unit tests

--- src/CodeBuilder.js
+++ src/CodeBuilder.js
@@ -6,24 +6,24 @@
  */
 export class CodeDictBuilder {
   constructor(codesDict) {
-    this.codes = codesDict;
+    this.codes = codesDict ? codesDict: {};
   }
 
   element(el) {
     const nameAttr = el.getAttribute("name");
     const codeValue = el.getAttribute("value");
-    if (nameAttr && nameAttr.match(/^([0-9]{1,3}:)?(parent_)?code/)) {
+    if (nameAttr && nameAttr.match(/^([0-9]{1,3}:)?(parent_)?code/i) && codeValue) {
       const namePrefix = nameAttr.split(":");
       const prefix = namePrefix[0];
-      const codeType = namePrefix[1];
       if (namePrefix.length === 2) {
+        const codeType = namePrefix[1].toLowerCase(); // parent_code or code
         // Store prefix in codes list
         if (!this.codes[prefix]) this.codes[prefix] = {};
         this.codes[prefix][codeType] = codeValue;
       } else {
         if (!this.codes[0]) this.codes[0] = {};
         // Allow to push a single code without prefix
-        this.codes[0][nameAttr] = codeValue;
+        this.codes[0][nameAttr.toLowerCase()] = codeValue;
       }
     }
   }
