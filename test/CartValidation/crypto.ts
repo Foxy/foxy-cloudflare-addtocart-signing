1f1f03d39b7c08a97368cb91ab13c33a2612e786
ndvo <nelson@ocastudios.com>
2021-01-19 17:27:20 -0300

    test: add regex signer tests

---
+++ test/CartValidation/crypto.ts
@@ -0,0 +1,46 @@
+/**
+ * Simple Mock Hmac class.
+ *
+ * Provides a sign method that
+ */
+export class MockHmac {
+  willFail: boolean;
+  private __codes;
+  subtle: {
+    importKey: () => string;
+    sign: () => Promise<string>;
+  };
+
+  constructor(secret: string, cryptoEngine: any, codes = {}) {
+    this.willFail = false;
+    this.__codes = codes;
+    this.subtle = {
+      importKey: () => "key",
+      sign: () => Promise.resolve("signed"),
+    };
+  }
+
+  sign() {
+    return this.willFail ? Promise.reject() : Promise.resolve("signed");
+  }
+
+  signUrl() {
+    return this.sign();
+  }
+
+  async signInput(el: Element) {
+    el.setAttribute("name", "signed");
+  }
+
+  async signOption(el: Element) {
+    el.setAttribute("value", "signed");
+  }
+
+  async signRadio(el: Element) {
+    await this.signOption(el);
+  }
+
+  async signTextArea(el: Element) {
+    await this.signInput(el);
+  }
+}
